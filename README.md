f848bc7823a597bee90aa3368c8e26831a82f233
witluk <witkowskilukaszz@gmail.com>
2017-05-24 11:54:58 +0200

    Initial commit
    base for project

---
+++ README.md
@@ -0,0 +1,1 @@
+# library-my-first-spring-boot-app
---
+++ src/test/java/com/witluk/LibraryMyFirstSpringBootAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.witluk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LibraryMyFirstSpringBootAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
